7c1a6c07ef77b64712458f648465e7408dc97667
Gul <meryemgltksz@gmail.com>
2023-09-13 16:57:49 +0300

    updated onClickFib func

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ const count = ref(0)
 let temp = 0
 let num = 0
 
-function fibonacci() {
+function onClickFib() {
   let num2 = this.count
   if(num2==0)
     this.count++
@@ -27,13 +27,13 @@ function fibonacci() {
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="fibonacci()">count is {{ count }}</button>
+    <button type="button" @click="onClickFib()">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
-
+ibo
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
